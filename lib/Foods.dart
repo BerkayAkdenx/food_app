ccb44d527df66aa8ad03d225506bd7f6eb533bd7
BerkayAkdenx <ismetberkayakdeniz@gmail.com>
2022-12-27 03:23:17 +0300

    mainpage is completed

---
+++ lib/Foods.dart
@@ -0,0 +1,8 @@
+class Foods{
+  late int foodId;
+  late String foodName;
+  late String foodImagesName;
+  late double foodPrize;
+
+  Foods(this.foodId, this.foodName, this.foodImagesName, this.foodPrize);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Foods.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,39 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Food Menu'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +32,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<List<Foods>> comeToFoods() async {
+    var foodList = <Foods>[];
+    var f1 = Foods(1, "Meatball", "kofte.png", 15.99);
+    var f2 = Foods(2, "Baklava", "baklava.png", 11.99);
+    var f3 = Foods(3, "Fanta", "fanta.png", 5.99);
+    var f4 = Foods(4, "Kadayif", "kadayif.png", 8.99);
+    var f5 = Foods(5, "Pasta", "makarna.png", 11.99);
+    var f6 = Foods(6, "Ayran", "ayran.png", 5.99);
+    foodList.add(f1);
+    foodList.add(f2);
+    foodList.add(f3);
+    foodList.add(f4);
+    foodList.add(f5);
+    foodList.add(f6);
+    return foodList;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: FutureBuilder<List<Foods>>(
+        future: comeToFoods(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var foodList = snapshot.data;
+            return ListView.builder(
+              itemCount: foodList!.length,
+              itemBuilder: (context, indeks) {
+                var food = foodList[indeks];
+                return Card(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                          height: 150,
+                          width: 150,
+                          child:
+                              Image.asset(("Images/${food.foodImagesName}"))),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            food.foodName,
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          Text(
+                            "${food.foodPrize} \u{20BA}",
+                            style: TextStyle(fontSize: 25, color: Colors.blue),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Icon(Icons.keyboard_arrow_right)
+                    ],
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center();
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
